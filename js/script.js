f9724e219d2cd091e9e57ebda9d063f6834260cb
“Renáta <koselak.renata87@gmail.com>
2024-08-05 11:38:10 +0200

    add to js day

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@
 const dateElement = document.querySelector('#date');
 const timeElement = document.querySelector('#time');
 
+function whichDay(){
+    const d = new Date();
+    const days = ["Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat"];
+    return document.querySelector('#day').innerHTML = days[d.getDay()];
+}
+
 setInterval (function(){
     dateElement.textContent = new Date().toLocaleDateString('hu-HU');
 })
@@ -11,6 +17,7 @@ function displayTime(){
     timeElement.textContent = new Date().toLocaleTimeString('hu-HU');
 }
 
+whichDay();
 displayTime();
 
 setInterval (displayTime, 1000);
